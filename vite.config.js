67714196ff2bfd9a36724bdafd2bf6c59fe1411e
Belen <elportalveterinario.arg@gmail.com>
2026-03-16 17:50:01 -0300

    Fix: agregar base path para deploy

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// Reemplaza 'El-Portal' por el nombre EXACTO de tu repositorio en GitHub
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: true, // Allow external connections
-    port: 5173, // Changed port to default Vite port
-    strictPort: true, // Ensure no port conflicts
-  }
+  base: '/El-Portal/', 
 })
-
-
