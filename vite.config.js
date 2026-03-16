ded7d5441c0c3606e283c9529dd9d795d3a4f1c9
Belen <elportalveterinario.arg@gmail.com>
2026-03-16 18:07:49 -0300

    Fix: repo publico y workflow correcto

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // Reemplaza 'El-Portal' por el nombre EXACTO de tu repositorio en GitHub
 export default defineConfig({
   plugins: [react()],
-  base: '/El-Portal/', 
+  base: '/El-Portal-/', 
 })
