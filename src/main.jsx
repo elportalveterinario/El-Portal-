020f31439f90a4300220fb057efd41a867dd7114
Belen <elportalveterinario.arg@gmail.com>
2026-03-16 18:49:39 -0300

    Fix: basename agregado en BrowserRouter

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/El-Portal-">
       <App />
     </BrowserRouter>
   </StrictMode>,
